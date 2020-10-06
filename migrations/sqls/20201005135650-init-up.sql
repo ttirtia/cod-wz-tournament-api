afd4aeadc5c6f302023525e832138fa2b7bfe898
Thibaut Tirtia <ttirtia@pm.me>
2020-10-06 18:17:28 +0200

    initial work on jwt auth
    rename 'pseudo' field to 'username'
    manage user creation and login
    define db primary key and unique fields

--- migrations/sqls/20201005135650-init-up.sql
+++ migrations/sqls/20201005135650-init-up.sql
@@ -3,12 +3,12 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Users table
 CREATE TABLE users (
-  id        UUID DEFAULT uuid_generate_v4 (),
-  pseudo    TEXT NOT NULL,
-  email     TEXT NOT NULL,
+  id        UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+  username  TEXT UNIQUE NOT NULL,
+  email     TEXT UNIQUE NOT NULL,
   password  TEXT NOT NULL,
   is_admin  BOOLEAN DEFAULT false
 );
 
 -- TODO: remove test data
-INSERT INTO users(pseudo, email, password) VALUES('test_user', 'test@example.com', 'pwd');
+INSERT INTO users(username, email, password) VALUES('test_user', 'test@example.com', 'pwd');
