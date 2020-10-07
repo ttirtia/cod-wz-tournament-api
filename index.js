7c05192a7e5f5fea7e1b6332960949acfd3710c0
Thibaut Tirtia <ttirtia@pm.me>
2020-10-07 17:21:14 +0200

    use const in index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
 
 console.log("Checking for database migrations...");
-// Always perform database migrations on startup
-let DBMigrate = require("db-migrate");
-let Server = require("./server");
 
-let dbmigrate = DBMigrate.getInstance(true);
-dbmigrate.up().then( () => {
-  Server.start()
+const DBMigrate = require("db-migrate");
+const Server = require("./server");
+
+// Always perform database migrations on startup
+const dbmigrate = DBMigrate.getInstance(true);
+dbmigrate.up().then(() => {
+  Server.start();
 });
