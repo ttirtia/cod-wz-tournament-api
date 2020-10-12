9ab029116e3775ab8e16d91c6327478656e76736
Thibaut Tirtia <ttirtia@pm.me>
2020-10-12 17:41:47 +0200

    add user creation/modification time and disable user registration

--- migrations/sqls/20201005135650-init-up.sql
+++ migrations/sqls/20201005135650-init-up.sql
@@ -3,9 +3,11 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Users table
 CREATE TABLE users (
-  id        UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
-  username  TEXT UNIQUE NOT NULL,
-  email     TEXT UNIQUE NOT NULL,
-  password  TEXT NOT NULL,
-  is_admin  BOOLEAN DEFAULT false
+  id                UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+  username          TEXT UNIQUE NOT NULL,
+  email             TEXT UNIQUE NOT NULL,
+  password          TEXT NOT NULL,
+  is_admin          BOOLEAN NOT NULL DEFAULT false,
+  creation_time     TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  modification_time TIMESTAMP WITH TIME ZONE
 );
