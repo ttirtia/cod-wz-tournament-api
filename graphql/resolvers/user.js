0447bf55c47946d48a938e82069b963f82ac1720
Thibaut Tirtia <ttirtia@pm.me>
2020-10-27 16:09:22 +0100

    remove registration mutation and add filter to finder

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { Op } = require("sequelize");
 const { User } = require("../../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -8,45 +9,51 @@ const JWT_TOKEN_TIMEOUT = "1d";
 
 module.exports = {
   Query: {
-    async users(root, args, { user }, info) {
+    async findUsers(root, { filter }, { user }, info) {
       if (!user || !user.isAdmin) throw new Error("Unauthorized");
 
-      return await User.findAll();
+      if (typeof filter === "undefined") {
+        return await User.findAll({
+          order: [["username", "ASC"]],
+          raw: true,
+        });
+      }
+
+      let queryFilter = [];
+
+      if (typeof filter.id !== "undefined") {
+        queryFilter.push({ id: { [Op.eq]: filter.id } });
+      }
+
+      if (typeof filter.username !== "undefined") {
+        queryFilter.push({
+          username: { [Op.iLike]: "%" + filter.username + "%" },
+        });
+      }
+
+      if (typeof filter.email !== "undefined") {
+        queryFilter.push({
+          email: { [Op.iLike]: "%" + filter.email + "%" },
+        });
+      }
+
+      if (typeof filter.isAdmin !== "undefined") {
+        queryFilter.push({
+          isAdmin: { [Op.eq]: filter.isAdmin },
+        });
+      }
+
+      return await User.findAll({
+        where: {
+          [Op.and]: queryFilter,
+        },
+        order: [["username", "ASC"]],
+        raw: true,
+      });
     },
   },
 
   Mutation: {
-    // Disable user registration
-    /*
-    async register(root, { email, username, password }, { user }, info) {
-      if (user) throw new Error("Already logged in");
-
-      try {
-        const res = await User.create({
-          username: username,
-          email: email,
-          password: password,
-          isAdmin: false,
-        });
-
-        return jwt.sign(
-          {
-            id: res.id,
-            username: res.username,
-            email: res.email,
-            isAdmin: res.isAdmin,
-          },
-          process.env.JWT_SIGNING_KEY,
-          {
-            expiresIn: JWT_TOKEN_TIMEOUT,
-          }
-        );
-      } catch (createError) {
-        throw new Error(createError);
-      }
-    },
-    */
-
     async login(root, { email, password }, { user }, info) {
       if (user) throw new Error("Already logged in");
 
