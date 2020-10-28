b1114822dea5bb345d0aee3f27159bd19bf4c77b
Thibaut Tirtia <ttirtia@pm.me>
2020-10-28 18:13:08 +0100

    initial work on tournaments: relationship with rosters only

---
+++ models/tournament.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Tournament extends Model {
+    static associate(models) {
+      Tournament.belongsTo(models.Roster, {
+        as: "roster"
+      });
+    }
+  }
+  Tournament.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      startDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      endDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      gameLimit: {
+        type: DataTypes.INTEGER,
+        defaultValue: -1,
+      },
+      isOpen: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Tournament",
+    }
+  );
+
+  return Tournament;
+};
