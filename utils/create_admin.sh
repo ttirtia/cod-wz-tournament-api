dacadb6493ee80f93086b2ad7dc3f500399eec2f
Thibaut Tirtia <ttirtia@pm.me>
2021-03-02 23:06:10 +0100

    format variables and add logging env variables

--- utils/create_admin.sh
+++ utils/create_admin.sh
@@ -3,10 +3,10 @@
 set -Eeuo pipefail
 
 readonly dir="$(dirname "$(realpath "$0")")"
-cd "${dir}/.."
+cd "$dir/.."
 
 readonly env_file=".env"
-. "${env_file}"
-export $(grep -E '^[A-Z]' "${env_file}" | cut -d= -f1)
+. "$env_file"
+export $(grep -E '^[A-Z]' "$env_file" | cut -d= -f1)
 
 node utils/js/create_admin.js
