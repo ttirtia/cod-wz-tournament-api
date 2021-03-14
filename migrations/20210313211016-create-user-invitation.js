b14955cd4b5e5c71eda25bfb65d9b3131d23d7fa
Thibaut Tirtia <ttirtia@pm.me>
2021-03-14 15:49:57 +0100

    prevent duplicate invitations

--- migrations/20210313211016-create-user-invitation.js
+++ migrations/20210313211016-create-user-invitation.js
@@ -7,7 +7,7 @@ module.exports = {
       await queryInterface.sequelize.query(
         `CREATE TABLE invitations (
           id             UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-          player_id      UUID REFERENCES players(id) ON DELETE CASCADE,
+          player_id      UUID REFERENCES players(id) ON DELETE CASCADE UNIQUE,
           is_admin       BOOLEAN NOT NULL DEFAULT false,
           valid_until    TIMESTAMP WITH TIME ZONE NOT NULL,
           created_at     TIMESTAMP WITH TIME ZONE NOT NULL,
